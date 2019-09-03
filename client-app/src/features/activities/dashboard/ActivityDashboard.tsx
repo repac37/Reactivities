cda0c8bc5a345eca584ca840b946c81ead104e0d
unknown <mikael_tofvesson@hotmail.com>
2019-09-03 10:25:18 +0200

    Section5

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -9,21 +9,51 @@ interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
   selectedActivity: IActivity | null;
+  setSelectedActivity: (activity: IActivity | null) => void;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+  deleteActivity: (id: string) => void;
 }
 
 const ActivityDashboard: React.FC<IProps> = ({
   activities,
   selectActivity,
-  selectedActivity
+  selectedActivity,
+  setSelectedActivity,
+  editMode,
+  setEditMode,
+  createActivity,
+  editActivity,
+  deleteActivity
 }) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList activities={activities} selectActivity={selectActivity} />
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+        />
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && <ActivityDetails activity={selectedActivity} />}
-        <ActivityForm />
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setSelectedActivity={setSelectedActivity}
+            setEditMode={setEditMode}
+          />
+        )}
+        {editMode && (
+          <ActivityForm
+            key={(selectedActivity && selectedActivity.id) || 0}
+            setEditMode={setEditMode}
+            activity={selectedActivity!}
+            createActivity={createActivity}
+            editActivity={editActivity}
+          />
+        )}
       </Grid.Column>
     </Grid>
   );
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Card, Image, Icon, Button } from "semantic-ui-react";
+import { Card, Image, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
     activity: IActivity;
+    setEditMode: (editMode: boolean) => void;
+    setSelectedActivity: (activity: IActivity | null) => void;
 
   }
 
-const ActivityDetails: React.FC<IProps> = ({activity}) => {
+const ActivityDetails: React.FC<IProps> = ({activity, setEditMode, setSelectedActivity}) => {
   return (
     <Card fluid>
       <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
@@ -22,8 +24,8 @@ const ActivityDetails: React.FC<IProps> = ({activity}) => {
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths={2}>
-            <Button basic color='blue' content='Edit' />
-            <Button basic color='grey' content='Cancel' />
+            <Button onClick={()=> setEditMode(true)} basic color='blue' content='Edit' />
+            <Button onClick={()=> setSelectedActivity(null)} basic color='grey' content='Cancel' />
         </Button.Group>
       </Card.Content>
     </Card>
